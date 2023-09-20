c70b93894bec7584fc86d10569092964f9606534
Hoang-Long <64343005+broripeu19@users.noreply.github.com>
2023-09-20 16:59:00 +0700

    Sendo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './assets/css/app.module.scss';
+import ProductListPage from './pages/ProductListPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import { CartProvider } from './context/CartContext'; 
+
+const cx = classNames.bind(styles);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CartProvider>
+        <Header />
+        <div className={cx('body')}>
+          <Routes>
+            <Route path="/" element={<ProductListPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </CartProvider>
+    </Router>
   );
 }
 
